0443656471123e6dde4a19c9122747356d8be876
David Beitey <david@davidjb.com>
2018-03-03 17:11:47 +1000

    Move cert formatting into post-hook so it runs after first issue too

--- post-hook.sh
+++ post-hook.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # This script runs after each call to acme.sh; for either success or failure
-# If domain-specific config is needed, use the first argument to this script:
-#   domain="$1"
 
 firewall_cfg="/etc/config/firewall"
 firewall_cfg_backup="$firewall_cfg.bak"
+certhome="/etc/lighttpd/certs"
+domain="$1"
 
+# Join cert and key into pem format for lighttpd
+cat "$certhome/$domain/$domain.cer" "$certhome/$domain/$domain.key" > "$certhome/$domain/$domain.pem"
+
+# Restore original firewall config
 mv "$firewall_cfg_backup" "$firewall_cfg"
-
 if ! /etc/init.d/firewall reload
 then
     echo 'Failed to reload firewall, aborting!'
--- renew-hook.sh
+++ renew-hook.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 # This script runs after each successful renewal of a certificate
 
-certhome="/etc/lighttpd/certs"
-domain="$1"
-
-# Join cert and key into pem for lighttpd
-cat "$certhome/$domain/$domain.cer" "$certhome/$domain/$domain.key" > "$certhome/$domain/$domain.pem"
-
 if ! /etc/init.d/lighttpd reload
 then
     echo 'Failed to reload lighttpd, aborting!'
