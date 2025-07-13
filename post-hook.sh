de85c53164cb9e22b4c21aff746349db7a890e1a
David Beitey <david@davidjb.com>
2025-07-13 12:18:21 +1000

    post-hook: iterate through nft handles for safety

--- post-hook.sh
+++ post-hook.sh
@@ -9,9 +9,12 @@
 #   Le_Domain=domain.example.com
 # These are not set on failure.
 
-# Remove firewall rules we set up earlier; use the comment to ensure only our rules are removed
-handle=$(nft --handle list chain inet fw4 input_wan | grep 'turris-omnia-tls acme.sh' | sed -nE 's/^.* # handle (.+)$/\1/p')
-nft delete rule inet fw4 input_wan handle "$handle"
+# Remove firewall rule we set up earlier; use the comment to ensure only our rule is removed
+handles=$(nft --handle list chain inet fw4 input_wan | grep 'turris-omnia-tls acme.sh' | sed -nE 's/^.* # handle (.+)$/\1/p')
+# $handles should only be a single entry but iterate for safety
+for handle in $handles; do
+  nft delete rule inet fw4 input_wan handle "$handle"
+done
 
 # Generate a .pem file containing both cert and key
 if [ -n "$CERT_PATH" ]; then
